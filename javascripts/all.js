7ee7c5392e9a436ce77c34d835a1fc202582c8b5
manga_osyo <manga.osyo@gmail.com>
2014-06-23 13:07:59 +0900

    Automated commit at 2014-06-23 04:07:59 UTC by middleman-deploy 0.2.4

--- javascripts/all.js
+++ javascripts/all.js
@@ -2,7 +2,8 @@ var AnimateDemoCtrl = function ($scope) {
 	$scope.model = {
 		visible: true,
 		scale: true,
-		move: true
+		move: true,
+		height: true
 	}
 	$scope.text1 = "The ngAnimate module provides support for JavaScript, CSS3 transition and CSS3 keyframe animation hooks within existing core and custom directives.";
 	$scope.text2 = "To see animations in action, all that is required is to define the appropriate CSS classes or to register a JavaScript animation via the myModule.animation() function. The directives that support animation automatically are: ngRepeat, ngInclude, ngIf, ngSwitch, ngShow, ngHide, ngView and ngClass. Custom directives can take advantage of animation by using the $animate service.";
